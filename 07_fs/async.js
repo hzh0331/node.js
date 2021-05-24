c7e257ad5f1cc4f6c089360f5a3d2afbc2214d50
ZihaoHan <972802149@qq.com>
2021-05-24 11:55:32 -0700

    fs async

---
+++ 07_fs/async.js
@@ -0,0 +1,35 @@
+const {readFile, writeFile} = require('fs')
+
+readFile('./first.txt', 'utf-8', (err, result)=>{
+    if(err){
+        console.log('err: ' + err)
+        return
+    }
+
+    const first = result
+
+    readFile('./first.txt', 'utf-8', (err, result)=>{
+        if(err){
+            console.log('err: ' + err)
+            return
+        }
+
+        const second = result
+
+        writeFile('./async-overwrite.txt', first + " " + second + "\n", (err, result)=>{
+            if(err){
+                console.log('err: ' + err)
+                return
+            }
+            console.log(result)
+        })
+
+        writeFile('./async-append.txt', first + " " + second + "\n", {flag:'a'}, (err, result)=>{
+            if(err){
+                console.log('err: ' + err)
+                return
+            }
+            console.log(result)
+        })
+    })
+})
--- 07_fs/sync.js
+++ 07_fs/sync.js
@@ -3,5 +3,5 @@ const {readFileSync, writeFileSync} = require('fs')
 const first = readFileSync('./first.txt', 'utf-8')
 const second = readFileSync('./second.txt', 'utf-8')
 
-writeFileSync('./overwrite.txt', first + " " + second + "\n")
-writeFileSync('./append.txt', first + " " + second + "\n", {flag:'a'})
+writeFileSync('./sync-overwrite.txt', first + " " + second + "\n")
+writeFileSync('./sync-append.txt', first + " " + second + "\n", {flag:'a'})
