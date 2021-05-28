84d2b7e27e30c0aed74cf44b44e03442b5e5cd4a
ZihaoHan <972802149@qq.com>
2021-05-27 21:30:57 -0700

    api

--- 2_Express/app.js
+++ 2_Express/app.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const path = require('path')
 
+const {products} = require('./data')
 const app = express()
 
-app.use(express.static('./public'))
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/api/product', (req, res)=>{
+    const productInfo = products.map((product)=>{
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+    res.json(productInfo)
 })
 
-app.get('/about', (req, res)=>{
-    res.status(200).send('About Page')
-})
-
-app.all('*', (req, res) =>{
-    res.status(400).send('<h1>404 Not Found</h1>')
+app.get('/api/product/:productID', (req, res)=>{
+    const {productID} = req.params
+    const product = products.find(product => product.id === Number(productID))
+    res.json(product)
 })
 
 app.listen(5000, ()=>{
