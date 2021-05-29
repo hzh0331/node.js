6afdebb90bdfe4cebcf65c9d78a07ae54df88d64
ZihaoHan <972802149@qq.com>
2021-05-28 21:50:08 -0700

    router

---
+++ 2_Express/Routers/people.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+
+let {people} = require('../data')
+
+router.get('/', (req, res)=>{
+    res.status(200).json({success:true, data:people})
+})
+
+router.post('/', (req, res)=>{
+    const {name} = req.body
+    // const person = {name}
+    if(!name){
+        return res.status(400).json({success:false, msg:'please provide name'})
+    }
+    res.status(201).json({success:true, person:name})
+})
+
+router.put('/:id', (req, res)=>{
+    const {id} = req.params
+    const {name} = req.body
+    const person = people.find(person => person.id === Number(id))
+    if(!person){
+        return res.status(404).json({success:false, msg:'person not exist'})
+    }
+    const newPeople = people.map(person =>{
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success:true, data: newPeople})
+})
+
+router.delete('/:id', (req, res)=>{
+    const {id} = req.params
+    const person = people.find(person => person.id === Number(id))
+    if(!person){
+        return res.status(404).json({success:false, msg:'person not exist'})
+    }
+    const newPeople = people.filter(person => person.id !== Number(id))
+    res.status(200).json({success:true, data:newPeople})
+})
+
+module.exports = router
--- 2_Express/app.js
+++ 2_Express/app.js
@@ -1,59 +1,15 @@
 const express = require('express')
 
-const {people} = require('./data')
+let peopleRouter = require('./Routers/people')
+let loginRouter = require('./Routers/login')
 
 const app = express()
 
 app.use(express.static("./methods-public"))
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-
-app.get('/api/people', (req, res)=>{
-    res.status(200).json({success:true, data:people})
-})
-
-app.post('/login', (req, res)=>{
-    const {name} = req.body
-    if(name){
-        res.status(200).send("welcome " + name)
-    }
-    res.status(401).send("error")
-})
-
-app.post('/api/people', (req, res)=>{
-    const {name} = req.body
-    // const person = {name}
-    if(!name){
-        return res.status(400).json({success:false, msg:'please provide name'})
-    }
-    res.status(201).json({success:true, person:name})
-})
-
-app.put('/api/people/:id', (req, res)=>{
-    const {id} = req.params
-    const {name} = req.body
-    const person = people.find(person => person.id === Number(id))
-    if(!person){
-        return res.status(404).json({success:false, msg:'person not exist'})
-    }
-    const newPeople = people.map(person =>{
-        if (person.id === Number(id)) {
-            person.name = name
-        }
-        return person
-    })
-    res.status(200).json({success:true, data: newPeople})
-})
-
-app.delete('/api/people/:id', (req, res)=>{
-    const {id} = req.params
-    const person = people.find(person => person.id === Number(id))
-    if(!person){
-        return res.status(404).json({success:false, msg:'person not exist'})
-    }
-    const newPeople = people.filter(person => person.id !== Number(id))
-    res.status(200).json({success:true, data:newPeople})
-})
+app.use('/api/people', peopleRouter)
+app.use('/login', loginRouter)
 
 app.listen(5000, ()=>{
     console.log("server is listening on port 5000")
